b9c9b14c8f3de75f3b149051235aee7d6bcc63f9
Hank Stennes <hpstennes@gmail.com>
2020-07-03 12:30:17 -0500

    Make WatermarkDocument more efficient

--- src/WatermarkDocument.java
+++ src/WatermarkDocument.java
@@ -14,27 +14,31 @@ public class WatermarkDocument extends DefaultStyledDocument {
     @Override
     public void insertString (int offset, String str, AttributeSet a) throws BadLocationException {
         super.insertString(offset, str, a);
-        setColors(getText(0, getLength()));
+        setColors(getText(0, getLength()), offset);
     }
 
     @Override
-    public void remove (int offs, int len) throws BadLocationException {
-        super.remove(offs, len);
-        setColors(getText(0, getLength()));
+    public void remove (int offset, int len) throws BadLocationException {
+        super.remove(offset, len);
+        setColors(getText(0, getLength()), offset);
     }
 
-    private void setColors(String text){
-        setCharacterAttributes(0, text.length(), attrBlack, false);
-        addColor(text, "[name]");
-        addColor(text, "[filename]");
+    private void setColors(String text, int offset){
+        int start = text.substring(0, offset).indexOf("\n") + 1;
+        int end = text.substring(offset).indexOf("\n");
+        if(end == -1) end = text.length();
+        setCharacterAttributes(start, end, attrBlack, false);
+        addColor(text, "[name]", start, end);
+        addColor(text, "[filename]", start, end);
     }
 
-    private void addColor(String text, String str){
-        int index = -1;
-        while(true){
-            index = text.indexOf(str, index + 1);
+    private void addColor(String text, String str, int start, int end){
+        int index = start;
+        while(index < end){
+            index = text.indexOf(str, index);
             if(index == -1) break;
             setCharacterAttributes(index, str.length(), attr, false);
+            index++;
         }
     }
 }
